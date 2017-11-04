e61da839557c6371a81eaf2aebe0b9e593cc8b58
Geoff <geoffchoy@outlook.com>
2017-11-04 23:41:34 +1100

    Added python merge sort and minor improvements to others.

--- languages/python/sorts.py
+++ languages/python/sorts.py
@@ -6,9 +6,16 @@
 
 import sys
 import argparse
+import logging
 
 
-def bubble_sort(l, verbose):
+def swap(l, x, y):
+    tmp = l[x]
+    l[x] = l[y]
+    l[y] = tmp
+
+
+def bubble_sort(l):
     i = 0
     j = 0
     swaps = 0
@@ -17,9 +24,7 @@ def bubble_sort(l, verbose):
     while i < len(l) and len(l) > 1:
         while j < len(l)-1:
             if l[j] > l[j+1]:
-                tmp = l[j+1]
-                l[j+1] = l[j]
-                l[j] = tmp
+                swap(l, j, j+1)
                 swaps += 1
                 total_swaps += 1
             j += 1
@@ -31,13 +36,12 @@ def bubble_sort(l, verbose):
             swaps = 0
         i += 1
         j = 0
-    if verbose:
-        print("Total comparisons: {0}".format(comps))
-        print("Total swaps: {0}\n".format(total_swaps))
+    logging.info("Total comparisons: {0}".format(comps))
+    logging.info("Total swaps: {0}\n".format(total_swaps))
 
 
 
-def insertion_sort(l, verbose):
+def insertion_sort(l):
     i = 0
     comps = 0
     swaps = 0
@@ -46,19 +50,16 @@ def insertion_sort(l, verbose):
         while j > 0:
             # Swap adjacent values
             if l[j] < l[j-1]:
-                tmp = l[j-1]
-                l[j-1] = l[j]
-                l[j] = tmp
+                swap(l, j, j-1)
                 swaps += 1
             j -= 1
             comps +=1
         i += 1
-    if verbose:
-        print("Total comparisons: {0}".format(comps))
-        print("Total swaps: {0}\n".format(swaps))
+    logging.info("Total comparisons: {0}".format(comps))
+    logging.info("Total swaps: {0}\n".format(swaps))
 
 
-def selection_sort(l, verbose):
+def selection_sort(l):
     i = 0
     comps = 0
     swaps = 0
@@ -70,23 +71,49 @@ def selection_sort(l, verbose):
                 min_index = j
             j += 1
             comps += 1
-        # Swap
-        tmp = l[min_index]
-        l[min_index] = l[i]
-        l[i] = tmp
+        # Swap the smallest val on the RHS with current key
+        if min_index != i:
+            swap(l, min_index, i)
+            swaps += 1
         i += 1
-        swaps += 1
-    if verbose:
-        print("Total comparisons: {0}".format(comps))
-        print("Total swaps: {0}\n".format(swaps))
+    logging.info("Total comparisons: {0}".format(comps))
+    logging.info("Total swaps: {0}\n".format(swaps))
 
 
-def merge_sort(l, verbose):
+def merge_sort(l):
     comps = 0
     swaps = 0
-    if verbose:
-        print("Total comparisons: {0}".format(comps))
-        print("Total swaps: {0}\n".format(swaps))
+    if len(l) > 1:
+        mid = len(l)//2 # floor division for python 3
+        left = l[:mid]
+        right = l[mid:]
+        logging.info("Split list\nLeft: {0}\nRight:{1}\n".format(left, right))
+
+        # Recursively call merge_sort on the split lists
+        merge_sort(left)
+        merge_sort(right)
+
+        # At this point we have hit a leaf node and returned
+        # We need to merge the two lists left, right
+        i = 0 # parent list
+        x = 0 # left index
+        y = 0 # right index
+        while x < len(left) and y < len(right):
+            if left[x] <= right[y]:
+                l[i] = left[x]
+                x += 1
+            else:
+                l[i] = right[y]
+                y += 1
+            i += 1
+
+        # Depending on which split list finished first, we override with the left-over
+        if x < len(left):
+            l[i:] = left[x:]
+        if y < len(right):
+            l[i:] = right[y:]
+        logging.info("Merged: {0}\n".format(l))
+
 
 
 
@@ -101,19 +128,27 @@ if __name__ == "__main__":
                         help="increase output verbosity")
     args = parser.parse_args()
 
+    # Create a very basic logger
+    if args.verbosity:
+        logger = logging.basicConfig(level=logging.INFO)
+
+
     # Generate an unsorted list
-    l = list(reversed(range(0, 40)))
+    l = list(reversed(range(0, 10)))
     print("Unsorted list: \n{0}\n".format(l))
 
     if args.sort_type == "bubble":
-        print("\nBubble sort!\n{0}".format("-"*30))
-        bubble_sort(l, args.verbosity)
+        print("Bubble sort!\n{0}\n".format("-"*30))
+        bubble_sort(l)
     elif args.sort_type == "insertion":
-        print("\nInsertion sort!\n{0}".format("-"*30))
-        insertion_sort(l, args.verbosity)
+        print("Insertion sort!\n{0}\n".format("-"*30))
+        insertion_sort(l)
     elif args.sort_type == "selection":
-        print("\nSelection sort!\n{0}".format("-"*30))
-        selection_sort(l, args.verbosity)
+        print("Selection sort!\n{0}\n".format("-"*30))
+        selection_sort(l)
+    elif args.sort_type == "merge":
+        print("Merge sort!\n{0}\n".format("-"*30))
+        merge_sort(l)
     else:
         print("Unknown selection sort - exiting...")
         sys.exit()
