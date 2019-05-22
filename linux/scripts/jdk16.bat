a9b48c21bb37b1e3b7574c7bda4d621deb6cae46
Geoff Choy <geoffchoy@outlook.com>
2019-05-22 18:40:20 +1000

    Added switch Java versions batch script for cmd

---
+++ linux/scripts/jdk16.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_45
+
+echo setting PATH
+set PATH=C:\Program Files\Java\jdk1.6.0_45\bin;%PATH%
+
+echo Display java version
+java -version
